732a6e3fd156f5bcaf03133e16ad6893e4b3e3cd
Abhishek Singh <singh.abhishek151019@gmail.com>
2025-07-20 17:23:46 +0530

    done npm

---
+++ NODEJS/npm.js
@@ -0,0 +1,10 @@
+// NPM is a package manager for Node.js packages, or modules if you like.
+// npm install package-name // install pkg locally
+// npm install -g package-name // install pkg globally
+// npm update package-name // update a pkg
+// npm update // update all
+// npm outdated // check outdated pkgs
+// npm uninstall package-name // uninstall pkg
+// npm uninstall -g package-name // uninstall pkg globally
+// npm uninstall --save package-name // uninstall and modify dep
+// npm uninstall --save-dev package-name // uninstall and modify dev dep
