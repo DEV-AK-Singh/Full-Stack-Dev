0ce6c285817e5ba882b03ee6c480c329d6938278
Abhishek Singh <singh.abhishek151019@gmail.com>
2025-07-20 17:24:17 +0530

    done npm scripts

---
+++ NODEJS/npm_scripts.js
@@ -0,0 +1,8 @@
+// NPM scripts are commands you define in your package.json file to automate tasks like:
+// {
+//   "scripts": {
+//     "start": "node index.js",
+//     "test": "echo \"Running tests...\" && exit 0",
+//     "dev": "nodemon index.js"
+//   }
+// }
