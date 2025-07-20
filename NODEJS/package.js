9b154a422c9653c282c14f1647c6abb19af994c4
Abhishek Singh <singh.abhishek151019@gmail.com>
2025-07-20 17:24:02 +0530

    done package

---
+++ NODEJS/package.js
@@ -0,0 +1,4 @@
+// package.json is a special file that describes your Node.js project.
+// It contains information about your app, such as its name, version, dependencies, scripts, and more.
+// This file is essential for managing and sharing Node.js projects, especially when using npm (Node Package Manager).
+// npm init // series of questions about project and generate a package.json file.
