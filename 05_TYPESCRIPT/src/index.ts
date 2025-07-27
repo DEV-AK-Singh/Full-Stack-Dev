72d2c42c1e307eeef1af0719751810ae3c57ec36
Abhishek Singh <singh.abhishek151019@gmail.com>
2025-07-27 15:36:56 +0530

    basics_completed

--- 05_TYPESCRIPT/src/index.ts
+++ 05_TYPESCRIPT/src/index.ts
@@ -1,3 +1,7 @@
-let firstName: string = "John";
-let lastName: string = "Doe";
-console.log(firstName + " " + lastName);
+// let firstName: string = "John";
+// let lastName: string = "Doe";
+// console.log(firstName + " " + lastName);
+
+// const name = "John"; // already block scoped variable
+// console.log(name);
+
